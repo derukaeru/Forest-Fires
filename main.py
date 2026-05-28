728efa76430834722a94d206988270231a102654
dalifyyy <d4lified@gmail.com>
2026-05-28 17:28:32 +0800

    .

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import pygame
-
 pygame.init()
 
 screen = pygame.display.set_mode((480, 360))
 pygame.display.set_caption("Forest Fire Simulation")
-
 running = True
+
+grid_width = 64
+grid_height = 64
+grid_map = []
+
+for i in range(grid_width):
+    for j in range(grid_height):
+        pass
+
 while running:
-    for event in pygame.event.get()
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # sim
 
 pygame.quit()
